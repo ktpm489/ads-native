e3e73fbfc87aeaa421afb45e5dd83fbad0b4d3f5
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2017-07-05 21:20:28 +0100

    adding more info on game state

--- store/reducers/game.js
+++ store/reducers/game.js
@@ -2,9 +2,17 @@ import moment from 'moment';
 import {NEW_GAME_STARTED} from '../actions';
 const initialState = {
     date: `10-06-${moment().format('YYYY')}`,
+    status: {
+        team: null,
+        trust: null
+    },
     events: [],
     importantMessages: [],
-    notifications: []
+    notifications: {
+        mails: [],
+        news: []
+    },
+    actions: []
 };
 
 export default function game(state = initialState, action = {}) {
